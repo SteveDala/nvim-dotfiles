00030e170b71299c31bf072e06b2ab782ea16556
Steven Dalamaras <sd@stevendalamaras.dev>
2025-09-06 20:12:26 +1000

    latest from wsl

--- init.lua
+++ init.lua
@@ -199,6 +199,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-q>', '<C-v>', { desc = 'Remap visual block mode (Windows)' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -336,6 +338,7 @@ require('lazy').setup({
   {
     'stevedala/jupytext.nvim',
     config = true,
+    enabled = false,
     -- lazy=false,
   },
   { -- Useful plugin to show you pending keybinds.
